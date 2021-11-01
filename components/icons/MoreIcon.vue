4f34bc45a31066e8cc58e113632bde81029eee43
Byron Garcia <byron.garcia@tars.dev>
2021-11-01 14:53:46 -0500

    Markup: Creación de los iconos del template

---
+++ components/icons/MoreIcon.vue
@@ -0,0 +1,14 @@
+<template>
+    <svg
+        class="fill-current text-cp3 w-16"
+        viewBox="0 0 32 32"
+        xmlns="http://www.w3.org/2000/svg">
+        <path d="M17 15V6H15V15H6V17H15V26H17V17H26V15H17Z"/>
+    </svg>
+</template>
+
+<script>
+export default {
+    name: "MoreIcon",
+}
+</script>
