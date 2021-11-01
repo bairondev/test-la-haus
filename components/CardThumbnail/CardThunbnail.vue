da07d11a70ceffb01415e93716a56746a8feb3be
Byron Garcia <byron.garcia@tars.dev>
2021-11-01 15:35:25 -0500

    CardThunbnail: creación del componente para las vista pequeña del listado de favoritos

---
+++ components/CardThumbnail/CardThunbnail.vue
@@ -0,0 +1,49 @@
+<template>
+    <!-- card -->
+    <div
+        class="mb-6">
+        <a href="#">
+        <figure
+            class="relative w-full h-48">
+            <img
+            class="absolute z-30 inline object-cover h-48 border border-white rounded-xl"
+            src="https://lh-sobreplanos-staging.imgix.net/uploads/real_estate_attachment/picture/1451171/hacienda_niquia_apartamentos_en_venta_de_2_3_hab_gallery_e4dd46fae78e09fb034c.jpg"/>
+            <img
+            class="absolute z-20 inline object-cover h-48 border border-white rounded-xl"
+            src="https://lh-sobreplanos-staging.imgix.net/uploads/real_estate_attachment/picture/1451171/hacienda_niquia_apartamentos_en_venta_de_2_3_hab_gallery_e4dd46fae78e09fb034c.jpg"/>
+            <!-- Mask Image -->
+            <div
+            class="absolute right-0 z-10 inline w-3/12 h-48" >
+            <div
+                class="relative">
+                <span
+                class="absolute flex w-full top-0 left-0 h-48 bg-cp1 opacity-70 z-10 border border-white rounded-xl justify-end items-center">
+                    <b
+                    class="font-bold block text-white flex-none w-full text-right pr-6 md:pr-0 md:text-center md:pl-3 lg:pl-5">
+                    +2
+                    </b>
+                </span>
+                <img
+                class="opacity-80 object-cover w-full h-48 rounded-xl"
+                src="https://lh-sobreplanos-staging.imgix.net/uploads/real_estate_attachment/picture/1451171/hacienda_niquia_apartamentos_en_venta_de_2_3_hab_gallery_e4dd46fae78e09fb034c.jpg"/>
+            </div>
+            </div>
+            <!-- Mask Image -->
+        </figure>
+        </a>
+        <div
+        class="mt-3">
+        <h4
+            class="text-cp1 text-lg font-silka2 font-semibold mb-2">
+            <a href="#">
+            Titulo
+            </a>
+        </h4>
+        <p
+            class="text-cp2 text-sm font-normal">
+            2 propiedades guardadas
+        </p>
+        </div>
+    </div>
+    <!-- card -->
+</template>
