a56a483c08e6d67ae073e281ffa13e2547cbd655
Byron Garcia <byron.garcia@tars.dev>
2021-11-01 16:09:50 -0500

    CardNew: creación del componente para poder crear nuevas listas

---
+++ components/CardNew/CardNew.vue
@@ -0,0 +1,32 @@
+<template>
+    <!-- card -->
+    <div
+        class="mb-7">
+        <a
+        href="#"
+        class="rounded-lg w-full bg-cp5 h-48 flex justify-center items-center">
+            <MoreIcon />
+        </a>
+        <div
+        class="mt-4">
+        <h4
+            class="text-cp3 text-lg text-center font-medium mb-2">
+            <a href="#">
+            Crear una nueva lista
+            </a>
+        </h4>
+        </div>
+    </div>
+    <!-- card -->
+</template>
+
+<script>
+import MoreIcon from '../icons/MoreIcon.vue';
+
+export default {
+    name: 'CardNew',
+    components: {
+        MoreIcon
+    }
+}
+</script>
