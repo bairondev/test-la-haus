775be86a987da528fe75e52060e2ff8dd49990f5
Byron Garcia <byron.garcia@tars.dev>
2021-11-01 14:55:18 -0500

    Markup: creacion html tarjetas lista de favoritos

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,170 @@
 <template>
-  <Tutorial/>
+  <main
+      class="max-w-5xl mx-auto">
+      <div class="mx-auto max-w-6xl">
+        <h2 class="font-silka2 font-semibold text-xl md:text-3xl text-center my-4 md:mt-14 mb-9 py-1">
+          Listas de favoritos
+        </h2>
+        <!-- cardList -->
+        <div
+          class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 md:gap-8 p-4">
+
+          <!-- card -->
+            <div
+              class="mb-6">
+              <a href="#">
+                <figure
+                  class="relative w-full h-48">
+                  <img
+                    class="absolute z-30 inline object-cover h-48 border border-white rounded-xl"
+                    src="https://lh-sobreplanos-staging.imgix.net/uploads/real_estate_attachment/picture/1451171/hacienda_niquia_apartamentos_en_venta_de_2_3_hab_gallery_e4dd46fae78e09fb034c.jpg"/>
+                  <img
+                    class="absolute z-20 inline object-cover h-48 border border-white rounded-xl"
+                    src="https://lh-sobreplanos-staging.imgix.net/uploads/real_estate_attachment/picture/1451171/hacienda_niquia_apartamentos_en_venta_de_2_3_hab_gallery_e4dd46fae78e09fb034c.jpg"/>
+                  <!-- Mask Image -->
+                  <div
+                    class="absolute right-0 z-10 inline w-3/12 h-48" >
+                    <div
+                      class="relative">
+                      <span
+                        class="absolute flex w-full top-0 left-0 h-48 bg-cp1 opacity-70 z-10 border border-white rounded-xl justify-end items-center">
+                          <b
+                            class="font-bold block text-white flex-none w-full text-right pr-6 md:pr-0 md:text-center md:pl-3 lg:pl-5">
+                            +2
+                          </b>
+                        </span>
+                      <img
+                        class="opacity-80 object-cover w-full h-48 rounded-xl"
+                        src="https://lh-sobreplanos-staging.imgix.net/uploads/real_estate_attachment/picture/1451171/hacienda_niquia_apartamentos_en_venta_de_2_3_hab_gallery_e4dd46fae78e09fb034c.jpg"/>
+                    </div>
+                  </div>
+                  <!-- Mask Image -->
+                </figure>
+              </a>
+              <div
+                class="mt-3">
+                <h4
+                  class="text-cp1 text-lg font-silka2 font-semibold mb-2">
+                  <a href="#">
+                    Titulo
+                  </a>
+                </h4>
+                <p
+                  class="text-cp2 text-sm font-normal">
+                  2 propiedades guardadas
+                </p>
+              </div>
+            </div>
+          <!-- card -->
+          
+          <!-- card -->
+            <div
+              class="mb-6">
+              <a href="#">
+                <figure
+                  class="relative w-full h-48">
+                  <img
+                    class="absolute z-30 inline object-cover h-48 border border-white rounded-xl"
+                    src="https://lh-sobreplanos-staging.imgix.net/uploads/real_estate_attachment/picture/1451171/hacienda_niquia_apartamentos_en_venta_de_2_3_hab_gallery_e4dd46fae78e09fb034c.jpg"/>
+                  <img
+                    class="absolute z-20 inline object-cover h-48 border border-white rounded-xl"
+                    src="https://lh-sobreplanos-staging.imgix.net/uploads/real_estate_attachment/picture/1451171/hacienda_niquia_apartamentos_en_venta_de_2_3_hab_gallery_e4dd46fae78e09fb034c.jpg"/>
+                  <!-- Mask Image -->
+                  <div
+                    class="absolute right-0 z-10 inline w-3/12 h-48" >
+                    <div
+                      class="relative">
+                      <span
+                        class="absolute flex w-full top-0 left-0 h-48 bg-cp1 opacity-70 z-10 border border-white rounded-xl justify-end items-center">
+                          <b
+                            class="font-bold block text-white flex-none w-full text-right pr-6 md:pr-0 md:text-center md:pl-3 lg:pl-5">
+                            +2
+                          </b>
+                        </span>
+                      <img
+                        class="opacity-80 object-cover w-full h-48 rounded-xl"
+                        src="https://lh-sobreplanos-staging.imgix.net/uploads/real_estate_attachment/picture/1451171/hacienda_niquia_apartamentos_en_venta_de_2_3_hab_gallery_e4dd46fae78e09fb034c.jpg"/>
+                    </div>
+                  </div>
+                  <!-- Mask Image -->
+                </figure>
+              </a>
+              <div
+                class="mt-3">
+                <h4
+                  class="text-cp1 text-lg font-silka2 font-semibold mb-2">
+                  <a href="#">
+                    Titulo
+                  </a>
+                </h4>
+                <p
+                  class="text-cp2 text-sm font-normal">
+                  2 propiedades guardadas
+                </p>
+              </div>
+            </div>
+          <!-- card -->
+          
+          <!-- card -->
+            <div
+              class="mb-6">
+              <a href="#">
+                <figure
+                  class="relative w-full h-48">
+                  <img
+                    class="absolute z-30 inline object-cover h-48 border border-white rounded-xl"
+                    src="https://lh-sobreplanos-staging.imgix.net/uploads/real_estate_attachment/picture/1451171/hacienda_niquia_apartamentos_en_venta_de_2_3_hab_gallery_e4dd46fae78e09fb034c.jpg"/>
+                  <img
+                    class="absolute z-20 inline object-cover h-48 border border-white rounded-xl"
+                    src="https://lh-sobreplanos-staging.imgix.net/uploads/real_estate_attachment/picture/1451171/hacienda_niquia_apartamentos_en_venta_de_2_3_hab_gallery_e4dd46fae78e09fb034c.jpg"/>
+                  <!-- Mask Image -->
+                  <div
+                    class="absolute right-0 z-10 inline w-3/12 h-48" >
+                    <div
+                      class="relative">
+                      <span
+                        class="absolute flex w-full top-0 left-0 h-48 bg-cp1 opacity-70 z-10 border border-white rounded-xl justify-end items-center">
+                          <b
+                            class="font-bold block text-white flex-none w-full text-right pr-6 md:pr-0 md:text-center md:pl-3 lg:pl-5">
+                            +2
+                          </b>
+                        </span>
+                      <img
+                        class="opacity-80 object-cover w-full h-48 rounded-xl"
+                        src="https://lh-sobreplanos-staging.imgix.net/uploads/real_estate_attachment/picture/1451171/hacienda_niquia_apartamentos_en_venta_de_2_3_hab_gallery_e4dd46fae78e09fb034c.jpg"/>
+                    </div>
+                  </div>
+                  <!-- Mask Image -->
+                </figure>
+              </a>
+              <div
+                class="mt-3">
+                <h4
+                  class="text-cp1 text-lg font-silka2 font-semibold mb-2">
+                  <a href="#">
+                    Titulo
+                  </a>
+                </h4>
+                <p
+                  class="text-cp2 text-sm font-normal">
+                  2 propiedades guardadas
+                </p>
+              </div>
+            </div>
+          <!-- card -->
+
+        </div>
+        <!-- cardList -->
+      </div>
+    </main>
 </template>
 
 <script>
-export default {}
+import MoreIcon from '../components/icons/MoreIcon.vue';
+
+export default {
+  components: {
+    MoreIcon
+  }
+}
 </script>
