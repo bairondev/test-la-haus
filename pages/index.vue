c288c1fbeb660390e0fb5b88ab11dcdead4e8d78
Byron Garcia <byron.garcia@tars.dev>
2021-11-02 02:33:30 -0500

    SEO: se implementa tag description

--- pages/index.vue
+++ pages/index.vue
@@ -73,13 +73,22 @@ export default {
             ? thumbnail[key] = urlImage
             : null
           });
-
       })
-
       this.load = false;
-
     })
-  
-  }
+  },
+  head() {
+      return {
+        title: this.title,
+        meta: [
+          // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'Los mejores apartamentos a solo un click'
+          }
+        ]
+      }
+    }
 }
 </script>
